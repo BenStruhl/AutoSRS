d8d3b41478e9f0a087648c204fc9034683eca57d
benstruhl <struhl.b@husky.neu.edu>
2018-07-06 14:06:06 -0400

    added some functionality for mainFuncs

--- src/funcs/mainFuncs.js
+++ src/funcs/mainFuncs.js
@@ -1,5 +1,20 @@
-import { greet, bye, rotate90, fs, readDir, appendParentNameDate, isTif} from "./funcs";
+import { greet, bye, rotate90, fs, readDir, appendParentNameDate, isTif, fs} from "./funcs";
+export const Robot = require("robotjs")
 
+
+// exports all files in a specified folder to printport
+// to be dropped into Srs
 export const exportToPaperPort = () => {
-    
+    var listOfAllFiles = readDir("Path to set folder");
+    var listToExportToPaperPort = [];
+    for(var file of listOfAllFiles) {
+        if(fs.lstatSync(file).isDirectory()) {
+            var listOfXRays = readDir(file);
+            for(var xRay of listOfXRays) {
+                if(isTif(xRay)) {
+                    
+                }
+            }
+        } 
+    }
 }
