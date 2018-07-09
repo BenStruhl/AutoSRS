0f6b1f28642b60328cc583efabc72f85b94531b7
AmrEl-Azizi <elaziziamr@gmail.com>
2018-07-09 15:06:57 -0400

    Fixed syntax errors in mainfuncs and added a test button

---
+++ funcs.js
@@ -0,0 +1,56 @@
+export const Jimp = require("jimp")
+export const fs = require('fs');
+export const path = require("path")
+
+
+export const greet = () => {
+  return "Hello World!";
+};
+
+export const bye = () => {
+  return "See ya!";
+};
+
+//Rotates an image 90 degrees, returning true if succesful.
+export const rotate90 = (path, newPath) => {
+  Jimp.read(path, function (err, lenna) {
+    if (err) throw err;
+    lenna.rotate(90)
+         .write(newPath); // save
+  });
+  return true;
+};
+
+//Returns a list of all files in a given directory
+export var readDir = () => {
+  const testFolder = './test_files';
+  var files = fs.readdirSync(testFolder);
+  return files;
+};
+
+//Determines if a given file is a bmp
+export const isTif = (path) => {
+  const s = path.lengt - 4;
+  var str = (path.substring(s)).toLowerCase();
+  if(str === (".tif"))
+    return true;
+  return false;
+}
+
+// Given a path to a file, renames the file by appending the parent folders
+// name and date to the front.
+export const appendParentNameDate = (pathGiven) => {
+  var parentNameRawList = path.dirname(pathGiven).split("/");
+  var parentName = parentNameRawList.pop();
+  parentNameRawList.push(parentName);
+  var parentNameDotRemoved = parentName.split(".").shift();
+  var parentNameDecomp = parentNameDotRemoved.split("_");
+  var newNameToAppend = "";
+  for(var i = 0; i < 3; i += 1) {
+    newNameToAppend += (parentNameDecomp.shift() + "_");
+  }
+  var basename = path.basename(pathGiven);
+  var pathName = parentNameRawList.join("/");
+  fs.renameSync(pathName + "/"  + basename, pathName + "/" + newNameToAppend + basename);
+  return  pathName + "/" + newNameToAppend + basename; 
+}
---
+++ mainFuncs.js
@@ -0,0 +1,41 @@
+import { greet, bye, rotate90, fs, readDir, appendParentNameDate, isTif} from "./funcs";
+
+export const robot = require("robotjs");
+export const printJS = require("printJS");
+
+
+// exports all files in a specified folder to printport
+// to be dropped into Srs
+export const exportToPaperPort = () => {
+    var listOfAllFiles = readDir("C:/Users/staff.SRS/Desktop/AutoSRS-master/test_files");
+    var listToExportToPaperPort = [];
+    for(var file of listOfAllFiles) {
+        if(fs.lstatSync(file).isDirectory()) {
+            var listOfXRays = readDir(file);
+            for(var xRay of listOfXRays) {
+                if(isTif(xRay)) {
+                    var newPath = appendParentNameDate(pathGiven);
+                    rotate90(pathGiven, newPath);
+                    listToExportToPaperPort.push(newPath);
+                }
+            }
+        } 
+    }
+
+    for(var file of listToExportToPaperPort) {
+        robot.setKeyboardDelay(0);
+        robot.setMouseDelay(0);
+        robot.keyTap("command");
+        robot.typeString("PaperPort");
+        robot.keyTap("enter")
+        for(var xRay in listToExportToPaperPort) {
+            printJS(xRay); 
+        }
+        // Call 3rd part function
+    }
+    // user robot.js to activate paperport 
+    // for each name in the list
+    //      print(file)
+    //      robojs(hit enter)   
+    // when everything is done stage3()
+}
